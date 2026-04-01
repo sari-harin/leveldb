6dab3607afb7eb1973c15e4f43f71723e26cc636
Seawaiin <1303sari@gmail.com>
2026-04-01 17:06:49 +0900

    message

--- db/autocompact_test.cc
+++ db/autocompact_test.cc
@@ -1,6 +1,7 @@
 // Copyright (c) 2013 The LevelDB Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file. See the AUTHORS file for names of contributors.
+//---------------------
 
 #include "gtest/gtest.h"
 #include "db/db_impl.h"
